e263a8b54356cf2da9c429a145531a613e06baff
Muniox <muniox20@gmail.com>
2022-06-27 23:06:46 +0200

    Initial Files from d01_Express

---
+++ d01_express/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const PORT = 3000;
+const ADDRESS = '127.0.0.1';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-type': 'text/html; charset=utf-8'
+    });
+    res.write('działa');
+   res.end();
+});
+
+app.listen(PORT, ADDRESS, () => {
+     console.log(`serwer działa na http://${ADDRESS}:${PORT}`);
+ });
+
+
+//po staremu jak było w node:
+// const { createServer } = require('http');
+// app.on('request', (req, res) => {
+//     res.writeHead(200, {
+//         'Content-type': 'text/html; charset=utf-8'
+//
+//     } );
+//     res.end('działa');
+// });
+//
+// app.listen(PORT, ADDRESS, () => {
+//     console.log(`serwer działa na http://${ADDRESS}:${PORT}`);
+// });
