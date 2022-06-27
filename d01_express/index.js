8dd5468b78672f188604548bd550fc9edef5dea4
Muniox <muniox20@gmail.com>
2022-06-27 23:13:08 +0200

    Additional d01_express

--- d01_express/index.js
+++ d01_express/index.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
    res.end();
 });
 
+app.get('/hello', (req, res) => {
+    console.log('cześć');
+    res.end();
+});
+
+app.post('/', (req, res) => {
+    console.log('dodaj nową książkę do listy');
+    res.end();
+});
+
 app.listen(PORT, ADDRESS, () => {
      console.log(`serwer działa na http://${ADDRESS}:${PORT}`);
  });
