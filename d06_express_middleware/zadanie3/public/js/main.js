50d1e7f41c18559eb583ee2f00609ea1b3b41a94
Muniox <muniox20@gmail.com>
2022-07-08 15:54:47 +0200

    finished excesaries 3

--- d06_express_middleware/zadanie3/public/js/main.js
+++ d06_express_middleware/zadanie3/public/js/main.js
@@ -1,7 +1,7 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('sendName');
 const checkButton = document.getElementById('checkSavedCookie');
-
+const showCookieName = document.getElementById('showCookieName');
 
 button.addEventListener('click', async () => {
     try {
@@ -29,6 +29,22 @@ checkButton.addEventListener('click', async () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ name: nameInput.value })
+        });
+        const data = await response.json();
+        alert(data.message);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+showCookieName.addEventListener('click', async () => {
+        try {
+        const response = await fetch('http://localhost:3000/cookie/show', {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
         });
         const data = await response.json();
         alert(data.message);
