969dee20eb6a36ecca438dd6db8c318792e440f0
Muniox <muniox20@gmail.com>
2022-07-15 18:44:32 +0200

    finish d13_express_konfigurator_ciastek

--- d13_express_konfigurator_ciastek/routes/home.js
+++ d13_express_konfigurator_ciastek/routes/home.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const homeRouter = express.Router();
+const {COOKIE_BASES, COOKIE_ADDONS} = require('../data/cookies-data');
 
 homeRouter
     .get('/', (req, res)=> {
-        res.render('home/index');
+        res.render('home/index', {
+            cookie: {
+                base: 'dark',
+                addons: ['coconut'],
+            },
+            addons: Object.entries(COOKIE_ADDONS),
+            bases: Object.entries(COOKIE_BASES),
+            //@TODO: We need something to know what are prices of selected base and addons...
+        });
     });
 
 module.exports = homeRouter;
