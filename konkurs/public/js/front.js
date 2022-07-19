4b5556dc7f1ad4920278085f39a8d2e9d19a5ed6
Muniox <muniox20@gmail.com>
2022-07-19 07:18:50 +0200

    code refactor

--- konkurs/public/js/front.js
+++ konkurs/public/js/front.js
@@ -17,72 +17,72 @@ sendTaskInput.addEventListener('keypress', async (event) => {
         });
         const data = await response.json();
 
-        //create element for append-------------------------
+        //element create for append
         const newItem = document.createElement("li");
-        //div
+        //div create
         const div = document.createElement("div");
         div.classList.add('view');
-        //input checkbox
+        //input checkbox create
         const input = document.createElement('input');
         input.setAttribute("type", "checkbox");
         input.setAttribute("data-id", `${data.id}`);
         input.classList.add('toggle');
+        //add event listener for checkbox
         input.addEventListener( 'click', async (event) => {
-
-        const response = await fetch(`/completed/${input.dataset.id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
+        
+            const response = await fetch(`/completed/${input.dataset.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
             
-        const data = await response.json();
-        if (data.toggle) {
-        //toggle check on li item
-        input.parentElement.parentElement.classList.toggle("completed");
-        !input.checked;
-        } else {
-            event.stopPropagation();
-        }
+            const data = await response.json();
+            if (data.toggle) {
+            //toggle check on li item
+                input.parentElement.parentElement.classList.toggle("completed");
+                !input.checked;
+            } else {
+                event.stopPropagation();
+            }
 
-        //count how many items are checked/done
-        counter.textContent = document.querySelectorAll('input[type="checkbox"]:checked').length.toString();
+            //count how many items are checked/done
+            counter.textContent = document.querySelectorAll('input[type="checkbox"]:checked').length.toString();
         });
-        //label
+        //label create
         const label = document.createElement('label');
         label.textContent = `${data.content}`;
-        //button x
+        //close button create 
         const button = document.createElement('Button');
         button.setAttribute("data-id",`${data.id}`);
         button.classList.add('destroy');
+        //close button add event listener
         button.addEventListener('click', async () => {
-        const id = button.dataset.id;
-        console.log(id);
-        const response = await fetch(`/delete/${id}`, {
-            method: 'DELETE',
-            headers: {
-                    'Content-Type': 'application/json'
-            },
+            const id = button.dataset.id;
+            console.log(id);
+            const response = await fetch(`/delete/${id}`, {
+                method: 'DELETE',
+                headers: {
+                        'Content-Type': 'application/json'
+                },
+            });
+            const data = await response.json();
+            if (data) {
+                button.parentElement.parentElement.remove();
+            }
         });
-    const data = await response.json();
-    if (data) {
-        button.parentElement.parentElement.remove();
-    }
-});
-        //inputEdit
+        //inputEdit create
         const inputEdit = document.createElement('input');
         inputEdit.classList.add('edit');
         inputEdit.setAttribute("value", `${data.content}`);
-
+        //append all elements
         newItem.appendChild(div);
         div.appendChild(input);
         div.appendChild(label);
         div.appendChild(button);
         newItem.appendChild(inputEdit);
         itemList.appendChild(newItem);
-    }
-    
-});
+    }});
 
 //count how many items are checked/done
 counter.textContent = document.querySelectorAll('input[type="checkbox"]:checked').length.toString();
