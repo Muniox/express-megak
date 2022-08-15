6fb7f4e7b710851856339396b0fcc417b3af351e
Muniox <muniox20@gmail.com>
2022-08-15 22:16:27 +0200

    change name for d22

--- d22_express_zadanie_podsumowujace/index.js
+++ d22_express_zadanie_podsumowujace/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {engine} = require('express-handlebars');
 const clientRouter = require('./router/client');
+const homeRouter = require('./router/home');
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
@@ -8,13 +9,12 @@ app.use(express.json());
 app.use(express.static('public'));
 app.engine('hbs', engine({
     extname: '.hbs',
-    helpers: 'handlebarsHelpers', //poprawić
+    //helpers: 'handlebarsHelpers',
 }));
 app.set('view engine', '.hbs');
 
-
+app.use('/', homeRouter);
 app.use('/client', clientRouter);
-
 
 app.listen( 3000, 'localhost', () => {
     console.log(`serwer nasłuchuje na http://localhost:3000`);
